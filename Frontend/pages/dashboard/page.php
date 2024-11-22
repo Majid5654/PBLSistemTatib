1c42ee1eba5061a249f054991820099cdee1ce61
xavierzaidane <xavierzaidane@gmail.com>
2024-11-22 20:00:12 +0700

    Updated Homepage

---
+++ Frontend/pages/dashboard/page.php
@@ -0,0 +1,133 @@
+<div class="prog-status">
+<div class="header">
+    <h4>learning Progress</h4>
+    <div class="tabs">
+        <a href="#" class="active">1Y</a>
+        <a href="#">6M</a>
+        <a href="#">3M</a>
+    </div>
+</div>
+
+<div class="details">
+    <div class="item">
+        <h2>3.45</h2>
+        <p>Current GPA</p>
+    </div>
+    <div class="separator"></div>
+    <div class="item">
+        <h2>4.78</h2>
+        <p>Class Average GPA</p>
+    </div>
+</div>
+
+<canvas class="prog-chart"></canvas>
+
+</div>
+
+<div class="popular">
+<div class="header">
+    <h4>Popular</h4>
+    <a href="#"># Data</a>
+</div>
+
+<img src="assets/podcast.jpg">
+<div class="audio">
+    <i class='bx bx-podcast'></i>
+    <a href="#">Poedcast: Mastering Data Visualization</a>
+</div>
+<p>Learn to create compelling visualizations with data.</p>
+<div class="listen">
+    <div class="author">
+        <img src="assets/profile.png">
+        <div>
+            <a href="#">Alex</a>
+            <p>Data Analyst</p>
+        </div>
+    </div>
+    <button>Listen<i class='bx bx-right-arrow-alt'></i></button>
+</div>
+
+</div>
+
+
+<div class="upcoming">
+
+<div class="header">
+    <h4>You may like it</h4>
+    <a href="#">July <i class='bx bx-chevron-down'></i></a>
+</div>
+
+<div class="dates">
+    <div class="item">
+        <h5>Mo</h5>
+        <a href="#">12</a>
+    </div>
+    <div class="item active">
+        <h5>Tu</h5>
+        <a href="#">13</a>
+    </div>
+    <div class="item">
+        <h5>We</h5>
+        <a href="#">14</a>
+    </div>
+    <div class="item">
+        <h5>Th</h5>
+        <a href="#">15</a>
+    </div>
+    <div class="item">
+        <h5>Fr</h5>
+        <a href="#">16</a>
+    </div>
+    <div class="item">
+        <h5>Sa</h5>
+        <a href="#">17</a>
+    </div>
+    <div class="item">
+        <h5>Su</h5>
+        <a href="#">18</a>
+    </div>
+</div>
+
+<div class="events">
+    <div class="item">
+        <div>
+            <i class='bx bx-time'></i>
+            <div class="event-info">
+                <a href="#">Data Science</a>
+                <p>10:00-11:30</p>
+            </div>
+        </div>
+        <i class='bx bx-dots-horizontal-rounded'></i>
+    </div>
+    <div class="item">
+        <div>
+            <i class='bx bx-time'></i>
+            <div class="event-info">
+                <a href="#">Machine Learning</a>
+                <p>13:30-15:00</p>
+            </div>
+        </div>
+        <i class='bx bx-dots-horizontal-rounded'></i>
+    </div>
+    <div class="item">
+        <div>
+            <i class='bx bx-time'></i>
+            <div class="event-info">
+                <a href="#">Beginner Python</a>
+                <p>11:30-13:00</p>
+            </div>
+        </div>
+        <i class='bx bx-dots-horizontal-rounded'></i>
+    </div>
+    <div class="item">
+        <div>
+            <i class='bx bx-time'></i>
+            <div class="event-info">
+                <a href="#">Introduction to SQL</a>
+                <p>10:00-11:30</p>
+            </div>
+        </div>
+        <i class='bx bx-dots-horizontal-rounded'></i>
+    </div>
+</div>
+</div>
