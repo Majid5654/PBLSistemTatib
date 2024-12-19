519d1707d2363fc93ea31178c0d1504f91aaae98
Majid5654 <3rwanmajid@gmail.com>
2024-12-19 10:15:23 +0700

    kosongan

--- Frontend/components/footer.php
+++
@@ -1,9 +0,0 @@
-<footer>
-        <div class="copyright">
-            <div class="separator"></div>
-            <p>
-                Copyright © 2024 AsmrProg, All Rights Reserved.
-            </p>
-            <div class="separator"></div>
-        </div>
-    </footer>
